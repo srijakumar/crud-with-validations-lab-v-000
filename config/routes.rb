bd834218d2f3e6b73e6d8ca4867167e96912daee
Srija S <srijakumar7@gmail.com>
2019-04-08 01:13:10 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,16 @@
 Rails.application.routes.draw do
+  get 'song/new'
+
+  get 'song/create'
+
+  get 'song/edit'
+
+  get 'song/update'
+
+  get 'song/destroy'
+
+  get 'song/index'
+
+  get 'song/show'
+
 end
